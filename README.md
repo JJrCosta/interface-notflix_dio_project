f237503730311d00deecc09f98bb5b546930704c
JJrCosta <jrpercosta@gmail.com>
2022-02-20 21:28:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# interface-notflix_dio_project
+ Digital Innovation One's - HTML Web Developer - Bootcamp - Project . A web page  inspired by netflix layout,
---
+++ recursos/js/main.js
@@ -0,0 +1,25 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:8,
+    nav:false,
+    responsive:{
+        0:{
+            items:1.1
+        },
+        400: {
+            items:2.1
+        },
+        600:{
+            items:3.1
+        },
+        1000:{
+            items:4.1
+        },
+        1300:{
+            items:5.1
+        },
+        1900:{
+            items:10.1
+        }
+    }
+})
